22079bc9a62345e0b5df56cc8d57c2e2b88e782b
arduino12 <arduino12@users.noreply.github.com>
2018-02-14 07:54:33 +0200

    rgb_mix_encoder: remove uart stuff

--- rgb_mix_encoder/rgb_mix_encoder.ino
+++ rgb_mix_encoder/rgb_mix_encoder.ino
@@ -1,10 +1,6 @@
 #include <FastLED.h>
 
 
-/* uart baud rate in bit per seconds */
-#define SERIAL_BAUD_RATE				(115200)
-/* set uart read timeout in milliseconds */
-#define SERIAL_TIMEOUT					(1)
 /* rgb channels count: red, green, blue */
 #define LED_DATA_PIN					(5)
 /* total amount of rgb channels */
